e5e11cd0f9ed1b69dab96b3b6910c8aaadc909ee
Ted Mathew dela Cruz <tedmdelacruz@gmail.com>
2018-03-03 21:31:31 +0800

    Revamped migrations and add basic portfolio pages

--- app/home/models.py
+++ app/home/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
 
 class HomePage(Page):
@@ -26,14 +27,50 @@ class HomePage(Page):
     ]
 
 
-class ContactIndexPage(Page):
-    header = RichTextField(blank=True)
+class PortfolioIndexPage(Page):
+    header = models.CharField(blank=False, max_length=128, default="")
+    content = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('header'),
+        FieldPanel('content'),
+    ]
+
+    def get_context(self, request):
+        context = super(PortfolioIndexPage, self).get_context(request)
+        context['portfolio_entries'] = PortfolioEntryPage.objects.child_of(self).live()
+        return context
+
+
+class PortfolioEntryPage(Page):
+    link = models.CharField(blank=True, max_length=256, default="")
+    description = RichTextField(blank=True)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('link'),
+        FieldPanel('description'),
+        ImageChooserPanel('image'),
+    ]
+
+
+class ContactPage(Page):
+    header = models.CharField(blank=False, max_length=128, default="")
+    content = RichTextField(blank=True)
+
     email = models.EmailField(blank=False, max_length=256, default="")
     github = models.CharField(blank=False, max_length=256, default="")
     linkedin = models.CharField(blank=False, max_length=256, default="")
 
     content_panels = Page.content_panels + [
         FieldPanel('header'),
+        FieldPanel('content'),
         FieldPanel('email'),
         FieldPanel('github'),
         FieldPanel('linkedin'),
